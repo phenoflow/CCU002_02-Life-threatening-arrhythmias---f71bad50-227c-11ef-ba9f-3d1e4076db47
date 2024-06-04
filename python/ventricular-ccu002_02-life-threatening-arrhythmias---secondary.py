cbf228e94b149aedf6dc2caf60d6329e88f8cc3f
Martin Chapman <contact@martinchapman.co.uk>
2024-06-04 15:17:28 +0100

    Update made by Phenoflow

---
+++ python/ventricular-ccu002_02-life-threatening-arrhythmias---secondary.py
@@ -0,0 +1,25 @@
+# CVD-COVID-UK consortium, William N Whiteley, et al., 2024.
+
+import sys, csv, re
+
+codes = [{"code":"I47.0","system":"icd10"},{"code":"I47.2","system":"icd10"},{"code":"I49.0","system":"icd10"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('ccu002_02-life-threatening-arrhythmias-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["ventricular-ccu002_02-life-threatening-arrhythmias---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["ventricular-ccu002_02-life-threatening-arrhythmias---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["ventricular-ccu002_02-life-threatening-arrhythmias---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
